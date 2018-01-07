688c8e6e237e5e6f7765766ad8068d857f371438
tomof <fffjt007@gmail.com>
2018-01-07 11:22:54 +0900

    initlal commit 3

---
+++ 3/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    about: './src/about.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.txt']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
